9522cdc35fe078b944aa172e0f57a12172024cbc
confused-kiwi <131565284+confused-kiwi@users.noreply.github.com>
2023-11-24 11:58:39 -0500

    done 8th week hw
    done wow so amazing

--- Level 3/blog/blog/views.py
+++ Level 3/blog/blog/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
 from django.urls import reverse_lazy # new
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 class HomePageView(ListView):
     model = Post
@@ -19,13 +19,23 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title','author','body']
+    
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class BlogUpdateView(LoginRequiredMixin, UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BlogDeleteView(LoginRequiredMixin, DeleteView): # new
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # new
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
